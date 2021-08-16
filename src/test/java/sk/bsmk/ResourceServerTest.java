0e819d566e99cf573eee550e196c8cdcc31dcf0e
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-08-16 14:52:39 +0200

    working test with wiremock and jwks

--- src/test/java/sk/bsmk/ResourceServerTest.java
+++ src/test/java/sk/bsmk/ResourceServerTest.java
@@ -3,9 +3,12 @@ package sk.bsmk;
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.security.web.header.Header;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -27,15 +31,18 @@ public class ResourceServerTest {
 
   private static WireMockServer wireMockServer;
 
-  private static final RSAKey jwk;
+  private static final RSAKey rsaJWK;
+  private static final JWSSigner signer;
 
   static {
     try {
-      jwk =
+      rsaJWK =
           new RSAKeyGenerator(2048)
               .keyUse(KeyUse.SIGNATURE)
               .keyID(UUID.randomUUID().toString())
               .generate();
+
+      signer = new RSASSASigner(rsaJWK);
     } catch (JOSEException e) {
       throw new RuntimeException(e);
     }
@@ -46,7 +53,8 @@ public class ResourceServerTest {
     wireMockServer = new WireMockServer(8081);
     wireMockServer.start();
     wireMockServer.stubFor(
-        get(urlEqualTo("/issuer/.well-known/openid-configuration")).willReturn(ok(jwk.toPublicJWK().toJSONString())));
+        get(urlEqualTo("/issuer/.well-known/jwks.json"))
+            .willReturn(ok("{\"keys\": [" + rsaJWK.toPublicJWK().toJSONString() + "]}")));
   }
 
   @AfterAll
@@ -56,14 +64,32 @@ public class ResourceServerTest {
   }
 
   @Test
-  public void thatSecuredResourcesCanBeAccessed() {
-    final String token = "TODO";
-    final RequestEntity<Void> request = RequestEntity.get(ResourceServerApp.PATH)
+  public void thatSecuredResourcesCanBeAccessed() throws Exception {
+    final String token = generateToken();
+    final RequestEntity<Void> request =
+        RequestEntity.get(ResourceServerApp.PATH)
             .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
             .build();
 
     final ResponseEntity<String> response = restTemplate.exchange(request, String.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(response.getBody()).isEqualTo(ResourceServerApp.DATA);
+  }
+
+  private String generateToken() throws Exception {
+    JWTClaimsSet claimsSet =
+        new JWTClaimsSet.Builder()
+            .subject("alice")
+            .issuer("http://localhost:8081/issuer")
+            .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+            .build();
+
+    SignedJWT signedJWT =
+        new SignedJWT(
+            new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID()).build(), claimsSet);
+
+    signedJWT.sign(signer);
+
+    return signedJWT.serialize();
   }
 }
