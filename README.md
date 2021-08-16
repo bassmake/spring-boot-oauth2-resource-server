da5991b6017f0ca64b33a8fefdfac752a7178eda
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-08-16 14:37:14 +0200

    added jwks endpoint

---
+++ README.md
@@ -0,0 +1,26 @@
+```
+http https://cognito-idp.us-east-1.amazonaws.com/us-east-1_B1z7NcuJg/.well-known/jwks.json
+```
+
+```json
+{
+    "keys": [
+        {
+            "alg": "RS256",
+            "e": "AQAB",
+            "kid": "C/6C+kGomjY6LbzPyYH6Hl6/zzXXLcr6IHxhbWLfc2s=",
+            "kty": "RSA",
+            "n": "is-fMJ0p371NhY8Q9h9869nDxdhJKe6EUpIpt4xqwhZRwZZfm_rgPK3oYuoEb6G334XDlucGBxMw706EeeT_9DpmAyckYvB23lfpvLfnppC4bP24drLknJ4PIJOoH1APqI-vwuOW05zhWZ4ZN7hq5tSXIDQ88RRaI-YRExhM6vrGupmfku0BgLEcdfroUl3UkmQvTbN-09-KYsCMmH0LMiDYP-v7w8b2S2OvgaRMIzsvcP43ar4j0GT6OAKct6nfcoGm9lduvbXaeypMIgs2-UJlK-WofOvRamuGsPmf9P1NYaeVQxnhIl_6iWRFgqQa6NbZav2ARiua0FzR2xZp4w",
+            "use": "sig"
+        },
+        {
+            "alg": "RS256",
+            "e": "AQAB",
+            "kid": "iz00ZO4dlLQU7hDLi8cGXcS+JoOOrrdvAz7cD6mS6wI=",
+            "kty": "RSA",
+            "n": "oywuMxAD25jyjVo_fbUlBcX1GLXrUWEV7pIw3q6GbntfiXlCSdyPcUbZhmVqGYAp2q2Ck7XZIWxVVx_T3_zSIzwtIEXizBichysSasssGnSJo5ZmL6d0TpAC7nDYMCOd5z6494GhTE_oLXkSgNV4deXrogCZx86ZfzkMfrEuOb0xuA2kS8MU3fpTJOFqOt5QIYfQVJVSLqEPaCdq6VbCAtu4wYq-ObhQS89Y0cAHHYeS63qIIF9HUwjfkFX7FGBjsCi6DTXSpVQQ6IyUC_9O9xJRbz_IWbRYIM3vY8yQSZoFBLkcibBUpVI-in4Oq9fauo8fJSRdzxW6ccJjG58SDQ",
+            "use": "sig"
+        }
+    ]
+}
+```
--- src/test/java/sk/bsmk/ResourceServerTest.java
+++ src/test/java/sk/bsmk/ResourceServerTest.java
@@ -1,22 +1,69 @@
 package sk.bsmk;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.web.header.Header;
+
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ResourceServerTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+  @Autowired private TestRestTemplate restTemplate;
 
-    @Test
-    public void thatSecuredResourcesCanBeAccessed() {
-        final ResponseEntity<String> response = restTemplate.getForEntity(ResourceServerApp.PATH, String.class);
-        assertThat(response.getBody()).isEqualTo(ResourceServerApp.DATA);
+  private static WireMockServer wireMockServer;
+
+  private static final RSAKey jwk;
+
+  static {
+    try {
+      jwk =
+          new RSAKeyGenerator(2048)
+              .keyUse(KeyUse.SIGNATURE)
+              .keyID(UUID.randomUUID().toString())
+              .generate();
+    } catch (JOSEException e) {
+      throw new RuntimeException(e);
     }
+  }
 
+  @BeforeAll
+  public static void initAuthorizationServer() {
+    wireMockServer = new WireMockServer(8081);
+    wireMockServer.start();
+    wireMockServer.stubFor(
+        get(urlEqualTo("/issuer/.well-known/openid-configuration")).willReturn(ok(jwk.toPublicJWK().toJSONString())));
+  }
+
+  @AfterAll
+  public static void teardownAuthorizationServer() {
+    wireMockServer.stop();
+    wireMockServer = null;
+  }
+
+  @Test
+  public void thatSecuredResourcesCanBeAccessed() {
+    final String token = "TODO";
+    final RequestEntity<Void> request = RequestEntity.get(ResourceServerApp.PATH)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .build();
+
+    final ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody()).isEqualTo(ResourceServerApp.DATA);
+  }
 }
